bcb2f731ca9c83f17ac12b7eab327a0e62aa2c22
SCIPIO666 <eaphoney@gmail.com>
2025-11-21 21:50:52 +0300

    calculating functions implementation

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const displayPanel=document.querySelector(".display");
+let clickedSteps=[];
+let latestResult;
+let calculationsMemory={};
+
 function getButton(classname){
     const button=document.querySelector(`.${classname}`);
     return button;
@@ -44,24 +48,31 @@ const buttons = {
     oneDecimal: getButton("r0-zero-decimal"),
     twoDecimal: getButton("r2-two-decimal")
 };
-class ArithmeticOperations{
-    constructor(a,b){
-        this.a=a;
-        this.b=b;
-    }
-add(){return this.a + this.b;}
-subtract(){return this.a - this.b;}
-multiply(){return this.a * this.b;}
-divide(){return this.a / this.b;}
-raiseToPower(){return this.a ** this.b;}
-squareRoot(){return this.a ** (1/this.b);}
+
+function initializeCalculator(){
+
 }
+
 class SpecialOperations{
     constructor(number,symbol){
         this.number=number;
         this.symbol=symbol;
     }
 }
+
+class ArithmeticOperations{
+    constructor(a,b){
+        this.a=a;
+        this.b=b;
+    }
+    add(){return this.a + this.b;}
+    subtract(){return this.a - this.b;}
+    multiply(){return this.a * this.b;}
+    divide(){return this.a / this.b;}
+    raiseToPower(){return this.a ** this.b;}
+    squareRoot(){return this.a ** (1/this.b);}
+}
+
 function calculate(a, b, operator) {
     let mathOperation = new ArithmeticOperations(a, b);
 
@@ -75,15 +86,45 @@ function calculate(a, b, operator) {
         default: throw new error("Error: Invalid operator");
     }
 }
-function clearDisplay(){
-    displayPanel.textContent="";
-}
-function display(content){
-    displayPanel.textContent=content;
+
+class Display{
+    constructor(displayPanel){
+        this.displayPanel=displayPanel;
+    }
+    clearDisplay(){
+        this.displayPanel.textContent="";
+    } 
+    renderDisplay(content){
+        this. displayPanel.textContent=content;
+    }
 }
 
-//let maths=new arithmeticOperations(3,4);
+const displays=new Display(displayPanel);
 
-//console.log(maths.add(),maths.subtract(),maths.multiply(),maths.divide());
-console.log(calculate(9,2,"√x"))
-display("successful display test");
+
+function limitCalculatedStepsToThree(){
+    const allNumbers=document.querySelectorAll(".numbers");
+    allNumbers.forEach(number=>{
+        number.addEventListener("click",e=>{
+            clickedSteps.length<3 ? clickedSteps.push(e.target.textContent)  : handleExtraSteps(clickedSteps,e.target.textContent);
+            displays.renderDisplay(displayPanel.textContent+number.textContent);
+        });
+    });
+}
+
+function handleExtraSteps(arrayOfSteps,latestStep){
+
+    if(arrayOfSteps.length==3){
+        savePreviousCalculationToMemory();//to save previous calculation and carry over result
+        arrayOfSteps.push(latestStep);
+        latestResult=calculate(arrayOfSteps[0],arrayOfSteps[2],arrayOfSteps[1]);
+        displays.renderDisplay(latestResult);
+        //the display to compute as soon as it has two operands and an operator so that the next input will be aggregated to the calculations chain
+    }else{
+        arrayOfSteps.push(latestStep);//arrayOfSteps  keeps track of buttons pressed and their implications,and determines end and begin of new calculation
+        latestResult=calculate(arrayOfSteps[0],arrayOfSteps[2],arrayOfSteps[1]);
+        displays.renderDisplay(latestResult);
+    }
+ 
+}
+
